90c723f5d954bf621457dd07e997f3969dd149f6
Melchor <patchimou14@gmail.com>
2016-02-21 21:04:52 +0100

    Ajout de commentaires

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ struct Param{
 	int size;
 };
 
+
+/* Affiche la grille sur le terminal */
 void print_plaque(float (*plaque)[taillePlaque]) {
 	for (int i=0; i<taillePlaque; i++) {
 		printf("\n");
@@ -30,6 +32,8 @@ void print_plaque(float (*plaque)[taillePlaque]) {
 	printf("\n");
 }
 
+
+/* Initialise la plaque avec TEMP_CHAUD au milieu et le reste a TEMP_FROID */
 void init_plaque(float (*plaque)[taillePlaque], int i_min, int i_max) {
 	for (int i=0; i<taillePlaque; i++) {
 		for (int j=0; j<taillePlaque; j++) {
@@ -42,11 +46,15 @@ void init_plaque(float (*plaque)[taillePlaque], int i_min, int i_max) {
 }
 
 
+/* Convertit la puissance passee en parametre en taille */
 int convertPowToSize(int puissance_taillePlaque) {
 	return pow(2,(puissance_taillePlaque+4));
 }
 
 
+/* Fais les calculs verticalement sur la grille a partir
+	de l'emplacement i et j passe en parametre ainsi que la valeur 
+	de la case du milieu. Return la valeur de la case du milieu */
 float vertical(float (*plaque_apres)[taillePlaque], float current_value, int i, int j) {
 	if ( (i-1) < 0 ) 
 		plaque_apres[i+1][j] += current_value/6;
@@ -59,12 +67,18 @@ float vertical(float (*plaque_apres)[taillePlaque], float current_value, int i, 
 	return current_value -= current_value/3;
 }
 
+
+/* methode principale de calcul de la grille, pour chaque case ayant une valeur
+	non nulle de temperature, on calcule ses valeurs verticale avec la fonction
+	vertical, puis on inscrit la valeur de la case centrale */
 void calculate_grid(float (*plaque_avant)[taillePlaque], float (*plaque_apres)[taillePlaque]) {
 	for (int i=0; i<taillePlaque; i++) {
 		for (int j=0; j<taillePlaque; j++) {
 			if (plaque_avant[i][j] != TEMP_FROID) {
+				// Si on sort de la grille a gauche
 				if ( (j-1) < 0 ) 
 					plaque_apres[i][j+1] += vertical(plaque_apres,(plaque_avant[i][j])/6, i, j+1);
+				// Si on sort de la grille a droite
 				else if ( (j+1) >= taillePlaque ) 
 					plaque_apres[i][j-1] += vertical(plaque_apres,(plaque_avant[i][j])/6, i, j-1); 
 				else {
@@ -74,28 +88,34 @@ void calculate_grid(float (*plaque_avant)[taillePlaque], float (*plaque_apres)[t
 				float value = plaque_avant[i][j] - plaque_avant[i][j]/3;
 				plaque_apres[i][j] += vertical(plaque_apres,value, i, j);
 			}
-			//print_plaque(plaque_apres);
 		}
 	}
 }
 
+
+/* Copie la matrice finale dans l'ancienne afin de pouvoir la reutiliser
+	en tant qu'ancienne plaque */
 void matrix_copy(float (*dest)[taillePlaque], float (*src)[taillePlaque]) {
 	memcpy(dest, src, taillePlaque*taillePlaque*sizeof(float));
 }
 
+
+/* fonction gerant la methode iterative */
 void iterative_way(float (*plaque_avant)[taillePlaque], float (*plaque_apres)[taillePlaque], int nbIterations) {
 	for (int i=0; i<nbIterations; i++) {
 		calculate_grid(plaque_avant, plaque_apres);
 		matrix_copy(plaque_avant, plaque_apres);
-		print_plaque(plaque_apres);
+		// Permet de reinitialiser la plaque a 0 
 		memset(plaque_apres,0,taillePlaque*taillePlaque*sizeof(float));
 		if(!MFLAG){
-			print_plaque(plaque_apres);
+			print_plaque(plaque_avant);
 		}
 	}
 }
 
 
+/* methode principale, c'est elle qui dit quelle methode sera lancee
+	en fonction des differents parametres recus */
 void simulation(int puissance_taillePlaque, int nbIterations, int nbThread) {
 	taillePlaque = convertPowToSize(puissance_taillePlaque);
 
@@ -116,15 +136,16 @@ void simulation(int puissance_taillePlaque, int nbIterations, int nbThread) {
 
 	init_plaque(plaque_avant, i_min, i_max);
 
-	if (nbThread == 1) {
+	if (nbThread == 1) 
 		iterative_way(plaque_avant, plaque_apres, nbIterations);
-	}
 	else {
 		printf("Pas encore géré !");
 		exit(0);
 	}
 }
 
+
+/* Affiche les options avec lesquelles on a lance le programme */
 void display_options(int nb_iterations,struct Param *program_step,struct Param *num_threads,struct Param *nb_case_per_line,bool MFLAG, bool AFLAG){
 	int index = 0;
 
@@ -164,9 +185,11 @@ void display_options(int nb_iterations,struct Param *program_step,struct Param *
 	printf("AFLAG: %d\n",AFLAG);
 }
 
+
+/* La recuperation des parametres se fait dans le main */
 int main(int argc, char *argv[]) {
 
-	int nb_iterations = 2;
+	int nb_iterations = 10000;
 
 	struct Param program_step;
 	program_step.size=0;
@@ -190,13 +213,9 @@ int main(int argc, char *argv[]) {
 			while (s_param[index] != '\0') {
 				//et on range les int dedans
 				char num = s_param[index] - '0';
-				//printf("%d\n", (int)num);
 				nb_case_per_line.p_array[index] = (int)num;
-				//printf("%d\n", nb_case_per_line.p_array[index]);
-				//printf("%d\n",nb_case_per_line[index]);
 				//on construit le tableau à la bonne taille
 				nb_case_per_line.p_array = realloc(nb_case_per_line.p_array,((int)strlen(s_param)*sizeof(int)));
-
 				index++;
 			}
 			nb_case_per_line.size = index;
@@ -245,12 +264,11 @@ int main(int argc, char *argv[]) {
 			}
 			num_threads.size = index;
 		}
-
 	}
 
-	if(!MFLAG){
+	if(!MFLAG) 
 		display_options(nb_iterations,&program_step,&num_threads,&nb_case_per_line,MFLAG,AFLAG);
-	}
+
 	simulation(0,nb_iterations,NB_MIN_THREAD);
 }
 
