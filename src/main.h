68a9e61a73da6b3d9998d4fb8d4ab126d25d8a2d
Matthieu Perrin <pm403977@polytech.unice.fr>
2016-03-06 15:09:28 +0100

    make...

--- src/main.h
+++ src/main.h
@@ -7,10 +7,7 @@
 Matthieu Perrin
 **/
 int taillePlaque;
-struct Param{
-	int *p_array;
-	int size;
-};
+
 void init_plaque(float (*plaque)[taillePlaque], int i_min, int i_max);
 void display_options(int nb_iterations,struct Param *program_step,struct Param *num_threads,struct Param *nb_case_per_line,bool MFLAG, bool AFLAG);
 void chercherMoyenne(double *time_spent);
